18e0b7132a3602eb0e07a4d2f74a86e52c68cdba
Perry Timinah <timnahwekalao@gmail.com>
2025-07-09 13:52:38 +0300

    Add simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,7 @@
 principal = 1000  # Principal amount
-rate = 5         # Rate of interest
+rate = 0.05        # Rate of interest
 time = 3          # Time in years
 
-interest = (principal * rate * time)/100   # Simple interest formula
-print(f"The simple interest is: {interest}")
+I = ("P * R * T")   # Simple interest formula
+simple_interest = (principal * rate * time)
+print(f"The simple interest is: {simple_interest}")
